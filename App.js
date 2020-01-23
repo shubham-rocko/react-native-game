6032ccfbbe10892b94338f7be37464e8f5ab3204
Shubham Singh <Shubham11.singh@paytm.com>
2020-01-23 10:20:45 +0530

    Start Game screen initialize and styled

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 import Header from './components/header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
 
   return (
     <View style={styles.screen}>
         <Header title="Guess a Number"  />
+        <StartGameScreen />
     </View>
   );
 }
