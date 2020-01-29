9447b7d9006345279a7cdb123c29fc6f7e42eff2
Shubham Singh <Shubham11.singh@paytm.com>
2020-01-29 10:40:32 +0530

    Custom image and its styling added

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,12 +1,20 @@
  import React from 'react';
- import { View, Text, StyleSheet, Button } from 'react-native';
+ import { View, StyleSheet, Button, Image } from 'react-native';
+
+ import BodyText from "../components/BodyText";
+ import TitleText from "../components/TitleText";
 
  const GameOver = (props) => {
      return (
          <View style={styles.screen}>
-            <Text>Game is Over Now!</Text>
-            <Text>Number of rounds: {props.roundsNumber}</Text>
-            <Text>User Number: {props.userNumber}</Text>
+            <TitleText>Game is Over Now!</TitleText>
+            <View style={styles.imageContainer}>
+                <Image source={require('../assets/success.png')} 
+                style={styles.image}
+                resizeMode="cover"/>
+            </View>
+            <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+            <BodyText>User Number: {props.userNumber}</BodyText>
             <Button title="NEW GAME" onPress={props.onRestart} />
          </View>
      );
@@ -17,6 +25,19 @@
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    imageContainer: {
+        borderRadius: 200,
+        borderWidth: 3,
+        borderColor: 'black',
+        width: '80%',
+        height: 300,
+        overflow: 'hidden',
+        marginVertical: 30
     }
  })
  
